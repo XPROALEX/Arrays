5bc3aea16d2029660a9ac6383ea3f0048df31d00
Alessandro <alex97av@gmail.com>
2023-07-18 19:12:57 +0200

    creazione array carBrans con elementi

---
+++ src/Testing.java
@@ -0,0 +1,15 @@
+//Define a testing class with a main() method where you: -Define an array of strings called carBrands that is populated with 5 famous car brand names
+//
+//        Print in console the number of items of the carBrands array
+//        Print in console the 3rd item of carBrands array -Declare an empty array of integers called primeNumbers with a predefined size of 6 items
+//        Iinsert one by one the values of the first 6 prime numbers, populating primeNumbers array -Print in console all the content of the already populated primeNumbers array
+
+
+public class Testing {
+    public static void main(String[] args) {
+        String[] carBrands={"BMW","Fiat","Alfa Romeo","Mercedes","Ford"};
+        System.out.println(carBrands.length);
+        System.out.println(carBrands[2]);
+    }
+
+}
