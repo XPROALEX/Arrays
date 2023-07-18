62c1266fa9e7f3774de3c661e849d64b83906d00
Alessandro <alex97av@gmail.com>
2023-07-18 19:42:40 +0200

    crezione array primeNumbers e array print

--- src/Testing.java
+++ src/Testing.java
@@ -5,11 +5,22 @@
 //        Iinsert one by one the values of the first 6 prime numbers, populating primeNumbers array -Print in console all the content of the already populated primeNumbers array
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Testing {
     public static void main(String[] args) {
         String[] carBrands={"BMW","Fiat","Alfa Romeo","Mercedes","Ford"};
         System.out.println(carBrands.length);
         System.out.println(carBrands[2]);
+        int [] primeNumbers=new int[6] ;
+        primeNumbers[0]=2;
+        primeNumbers[1]=3;
+        primeNumbers[2]=5;
+        primeNumbers[3]=7;
+        primeNumbers[4]=11;
+        primeNumbers[5]=13;
+        System.out.println(Arrays.toString(primeNumbers));
     }
 
 }
